686cac4e9d0ad8b5b8716a359b2e2c9b7170dfb3
Shilov Vlad <omgovich@ya.ru>
2013-10-15 11:32:18 +0400

    Убраны outline у полей ввода

--- includes/head.php
+++ includes/head.php
@@ -5,8 +5,7 @@
 	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
 	<!-- Stylesheet -->
 	<link href="static/<?=$version?>/css/reset.css" rel="stylesheet" />
-	<link href="static/<?=$version?>/css/template.css" rel="stylesheet" />
-	<link href="static/<?=$version?>/css/blocks.css" rel="stylesheet" />
+	<link href="static/<?=$version?>/css/style.css" rel="stylesheet" />
 	<!--[if lt IE 9]><link href="static/<?=$version?>/css/ie.css" rel="stylesheet" /><![endif]-->
 	<!--[if lt IE 9]><script src="static/<?=$version?>/js/html5.js"></script><![endif]-->
 </head>
