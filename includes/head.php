be2a6f284c8c8a9cdd706ab4ba8aa7b4658413ff
Shilov Vlad <omgovich@ya.ru>
2014-01-28 17:41:42 +0400

    Исключен ie.css, перестроен head.php

--- includes/head.php
+++ includes/head.php
@@ -1,15 +1,13 @@
 <head>
 	<? $version = '1.0'; ?>
+	<title>Page title</title>
+	<!-- Meta -->
 	<meta charset="utf-8">
 	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="google" content="notranslate">
-	<title>Page title</title>
 	<!-- Stylesheet -->
 	<link href="static/<?=$version?>/css/reset.css" rel="stylesheet">
 	<link href="static/<?=$version?>/css/style.css" rel="stylesheet">
-	<!--[if lt IE 9]>
-		<link href="static/<?=$version?>/css/ie.css" rel="stylesheet" />
-		<script src="static/<?=$version?>/js/html5.js"></script>
-	<![endif]-->
+	<!--[if lt IE 9]><script src="static/<?=$version?>/js/html5.js"></script><![endif]-->
 </head>
--- includes/js.php
+++ includes/js.php
@@ -1,5 +1,3 @@
 <script src="static/<?=$version?>/js/jquery.js"></script>
 <script src="static/<?=$version?>/js/script.js"></script>
-<!--[if lt IE 9]>
-	<script src="static/<?=$version?>/js/IE9.js"></script>
-<![endif]-->
+<!--[if lt IE 9]><script src="static/<?=$version?>/js/ie9.js"></script><![endif]-->
