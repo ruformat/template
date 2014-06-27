501517a3cd2bb0cd6641249aaaea679aa26cfa50
Yami Enzeru <funky-angel@mail.ru>
2014-06-27 11:49:06 +0400

    autocomplete

--- static/js/init.js
+++ static/js/init.js
@@ -21,4 +21,70 @@ $.fn.ini = function(){
 	
 	/* scroll bar */
 	$('.scroll_bar').jScrollPane();
+	
+	
+};
+
+$.fn.cart = function(){
+
+	$('.autocomplete').each(function(){
+		var $value_input = $(this),
+			$data_input = $("<input/>", {
+				  type: "hidden",
+				  name: $value_input.attr('name'),
+				  value: $value_input.val()
+			}),
+			$bg_input = $("<input/>", {
+				  type: "text",
+				  tabindex: "-1",
+				  readonly: "readonly",
+				  value: $value_input.data('val'),
+				  autocomplete: "off",
+				  class: $value_input.attr('class'),
+				  size: $value_input.attr('size'),
+				  style: 'width:'+$value_input.css('width')+';position:absolute;'
+			}),
+			url = $value_input.data('url');
+		
+		$data_input.insertAfter($value_input);
+		$bg_input.insertAfter($data_input);
+		$value_input.attr('name', $value_input.attr('name') + '_val');
+		$value_input.val($value_input.data('val'));
+		
+		$value_input.autocomplete({
+			serviceUrl: url,
+			paramName: 'search',
+			autoSelectFirst: true,
+			deferRequestBy: '700',
+			containerClass: 'autocomplete-suggestions',
+			appendTo: $value_input.parent(),
+			noCache: true,
+			onSearchComplete: function(query, suggestions){
+				var $list = $value_input.parent().find('.autocomplete-suggestions').children();
+				$list.each(function(){
+					var $suggestion = $(this),
+						suggestion_key = $suggestion.data('index');
+						
+					$suggestion.mouseover(function(){
+						$bg_input.val(suggestions[suggestion_key].value);
+						$data_input.val(suggestions[suggestion_key].data);
+					});
+				});
+				
+				if(suggestions.length){
+					$bg_input.val(suggestions[0].value);
+					$data_input.val(suggestions[0].data);
+				}
+				else{
+					$bg_input.val('');
+					$data_input.val('');
+				}
+			},
+			onSelect: function(suggestion){
+				$bg_input.val(suggestion.value);
+				$data_input.val(suggestion.data);
+			}
+		});
+	});
+	
 };
